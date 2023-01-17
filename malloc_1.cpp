24c7ec0deb8b170bb0cc03ef5b7b2730cb2f6b6f
yoavgilad04 <yoavgilad1995g@gmail.com>
2023-01-17 12:07:14 +0200

    fix

--- malloc_1.cpp
+++ malloc_1.cpp
@@ -1,20 +1,20 @@
-//#include <stdio.h>
-//#include <unistd.h>
-//
-//#define MAX_SIZE (1e8)
-//#define PAGE_SIZE (4096)
-//
-//void* smalloc(size_t size){
-//    if(size<=0 || size > MAX_SIZE) return nullptr;
-//    //intptr_t increment =  (size + (PAGE_SIZE - size % PAGE_SIZE)) ;
-//
-//    //append heap
-//    intptr_t p = (intptr_t)sbrk(size);
-//
-//    if(p == -1){
-//        return nullptr;
-//    }
-//
-//    return (void*)(p);
-//}
-//
+#include <stdio.h>
+#include <unistd.h>
+
+#define MAX_SIZE (1e8)
+#define PAGE_SIZE (4096)
+
+void* smalloc(size_t size){
+    if(size<=0 || size > MAX_SIZE) return nullptr;
+    //intptr_t increment =  (size + (PAGE_SIZE - size % PAGE_SIZE)) ;
+
+    //append heap
+    intptr_t p = (intptr_t)sbrk(size);
+
+    if(p == -1){
+        return nullptr;
+    }
+
+    return (void*)(p);
+}
+
