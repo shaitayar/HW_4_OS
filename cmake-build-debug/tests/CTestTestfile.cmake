650b7e70220b5550a1dfd04c1df8195b5d1e79bc
yoavgilad04 <110780656+yoavgilad04@users.noreply.github.com>
2023-01-17 10:36:06 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/yoavg/Desktop/HW_4_OS/tests
-# Build directory: C:/Users/yoavg/Desktop/HW_4_OS/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/yoavg/Desktop/HW_4_OS/cmake-build-debug/tests/malloc_1_test_include-b12d07c.cmake")
-include("C:/Users/yoavg/Desktop/HW_4_OS/cmake-build-debug/tests/malloc_2_test_include-b12d07c.cmake")
-subdirs("../_deps/catch2-build")
